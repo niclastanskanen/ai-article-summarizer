8db956c7b6bcc1d78f01e4a6f32c0711ae61ab9d
Niclas Tanskanen <niclastanskanen@airsomtorniclas.home>
2023-04-24 10:59:16 +0200

    add history link from prev search in Demo.jsx

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect } from 'react';
 
-import { copy, linkIcon, loader, tick } from "../assets";
+import { copy, linkIcon, loader, tick } from '../assets';
 import { useLazyGetSummaryQuery } from '../services/article';
 
 const Demo = () => {
@@ -15,7 +15,7 @@ const Demo = () => {
   useEffect(() => {
     const articlesFromLocalStorage = JSON.parse(localStorage.getItem('articles'));
 
-    if(articlesFromLocalStorage) {
+    if (articlesFromLocalStorage) {
       setAllArticles(articlesFromLocalStorage);
     }
   }, []);
@@ -25,48 +25,59 @@ const Demo = () => {
 
     const { data } = await getSummary({ articleUrl: article.url });
 
-    if(data?.summary) {
+    if (data?.summary) {
       const newArticle = { ...article, summary: data.summary };
-      const updatedAllArticles = [newArticle, ...AllArticles];
+      const updatedAllArticles = [newArticle, ...allArticles];
 
       setArticle(newArticle);
       setAllArticles(updatedAllArticles);
 
       localStorage.setItem('articles', JSON.stringify(updatedAllArticles));
     }
-  }
+  };
 
   return (
     <section className="mt-16 w-full max-w-xl">
       <div className="flex flex-col w-full gap-2">
-        <form
-          className="relative flex justify-center items-center"
-          onSubmit={handleSubmit}
-        >
-        <img 
-          src={linkIcon}
-          alt="link_icon"
-          className="absolute left-0 my-2 ml-3 w-5"
-        />
-        <input 
-          type="url"
-          placeholder="Paste your link here"
-          value={article.url}
-          onChange={(e) => setArticle({ ...article, url: e.target.value })}
-          required
-          className="url_input peer"
-        />
-        <button
-          type="submit"
-          className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"
-        >
-          ↵
-        </button>
+        <form className="relative flex justify-center items-center" onSubmit={handleSubmit}>
+          <img src={linkIcon} alt="link_icon" className="absolute left-0 my-2 ml-3 w-5" />
+          <input 
+            type="url"
+            placeholder="Paste your link here"
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
+            required
+            className="url_input peer" 
+          />
+          <button
+            type="submit"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700">
+            ↵
+          </button>
         </form>
 
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+          {allArticles.map((item, index) => (
+            <div
+              key={`link-${index}`}
+              onClick={() => setArticle(item)}
+              className="link_card">
+              <div className="copy_btn">
+                <img
+                  src={copy}
+                  alt="copy_icon"
+                  className="w-[40%] h-[40%] object-contain" 
+                />
+              </div>
+              <p className='flex-1 font-satoshi text-blue-700 font-medium text-sm truncate'>
+                {item.url}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Demo
+export default Demo;
