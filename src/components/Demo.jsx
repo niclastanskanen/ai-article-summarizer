a4466ef98136713345a4fcb96636fb89392f21c6
Niclas Tanskanen <niclastanskanen@airsomtorniclas.home>
2023-04-24 11:13:45 +0200

    add copy function to article url

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -9,6 +9,7 @@ const Demo = () => {
     summary: '',
   });
   const [allArticles, setAllArticles] = useState([]);
+  const [copied, setCopied] = useState("");
 
   const [getSummary, { error, isFetching }] = useLazyGetSummaryQuery();
 
@@ -36,6 +37,12 @@ const Demo = () => {
     }
   };
 
+  const handleCopy = (copyUrl) => {
+    setCopied(copyUrl);
+    navigator.clipboard.writeText(copyUrl);
+    setTimeout(() => setCopied(false), 3000);
+  }
+
   return (
     <section className="mt-16 w-full max-w-xl">
       <div className="flex flex-col w-full gap-2">
@@ -62,9 +69,12 @@ const Demo = () => {
               key={`link-${index}`}
               onClick={() => setArticle(item)}
               className="link_card">
-              <div className="copy_btn">
+              <div 
+                className="copy_btn"
+                onClick={() => handleCopy(item.url)}
+              >
                 <img
-                  src={copy}
+                  src={copied === item.url ? tick : copy}
                   alt="copy_icon"
                   className="w-[40%] h-[40%] object-contain" 
                 />
@@ -94,7 +104,9 @@ const Demo = () => {
                     Article <span className='blue_gradient'>Summary</span>
                   </h2>
                   <div className='summary_box'>
-                    <p>{article.summary}</p>
+                    <p className='font-inter font-medium text-sm text-gray-700'>
+                      {article.summary}
+                    </p>
                   </div>
                 </div>
               )
