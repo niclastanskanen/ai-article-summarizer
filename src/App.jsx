b9879c8490e40b659085a3fbfe1f226a30a9a5be
Niclas Tanskanen <niclastanskanen@airsomtorniclas.home>
2023-04-25 10:22:02 +0200

    add signin signup button if logged out and userbutton if logged in in NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,31 @@ import Hero from './components/Hero';
 import Demo from './components/Demo';
 
 import './App.css';
-import { ClerkProvider } from '@clerk/clerk-react';
+import { ClerkProvider, SignedIn, SignedOut } from '@clerk/clerk-react';
 import NavBar from './components/NavBar';
+import Landing from './components/Landing';
 
 const clerkPubKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 const App = () => {
   return (
     <ClerkProvider publishableKey={clerkPubKey}>
-    <main>
-      <div className="main">
-        <div className="gradient" />
-      </div>
-      <div className="app">
-        <NavBar />
-        <Hero />
-        <Demo />
-      </div>
-    </main>
+      <main>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+        <div className="app">
+          <SignedIn>
+            <NavBar />
+            <Hero />
+            <Demo />
+          </SignedIn>
+          <SignedOut>
+            <NavBar />
+            <Landing />
+          </SignedOut>
+        </div>
+      </main>
     </ClerkProvider>
   );
 };
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,19 @@
 import { logo } from '../assets';
-import { SignInButton, SignUpButton } from '@clerk/clerk-react';
+import { SignInButton, SignUpButton, UserButton, useClerk } from '@clerk/clerk-react';
 
 const NavBar = () => {
+    const { user } = useClerk();
+
   return (
     <nav className="flex justify-between items-center w-full mb-10 pt-3">
       <img src={logo} alt="clearyfiAi_logo" className="w-28 object-contain" />
       <div className="flex justify-end items-center">
+      {user ? (
+        <div className="ml-4">
+          <UserButton />
+        </div>
+      ) : (
+        <>
         <SignUpButton mode="modal">
           <button className="btn rounded black_btn">Sign Up</button>
         </SignUpButton>
@@ -14,6 +22,8 @@ const NavBar = () => {
             <button className="btn">Sign In</button>
           </SignInButton>
         </div>
+        </>
+        )}
       </div>
     </nav>
   );
